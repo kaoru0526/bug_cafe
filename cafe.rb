1f189484cc05ee2fd8b69fe9d89d2cb470b7dfe0
“kaoru.am” <intel.uuuk@gmail.com>
2024-11-18 20:54:39 +0900

    between?を1つにまとめて定義・変数の変更

--- cafe.rb
+++ cafe.rb
@@ -14,30 +14,33 @@ FOODS = [
   { name: 'ホットサンド', price: 410 }
 ].freeze
 
+def ask_for_order_number(menus)
+  loop do
+    print 'メニュー番号を入力してください > '
+    order_number = gets.to_i - 1
+    if order_number.between?(0, menus.size - 1)
+      return order_number
+    else
+      puts "無効な入力です。1〜#{menus.size}の範囲で選んでください。"
+    end
+  end
+end
+
 def take_order(menus)
   menus.each.with_index(1) do |menu, i|
     puts "(#{i})#{menu[:name]}: #{menu[:price]}円"
   end
 
-  order_number = nil
-  until order_number && order_number.between?(0, menus.size - 1)
-    print 'メニュー番号を入力してください > '
-    order_number = gets.to_i - 1
-    unless order_number.between?(0, menus.size - 1)
-      puts "無効な入力です。1〜#{menus.size}の範囲で選んでください。"
-    end
-  end
-
+  order_number = ask_for_order_number(menus)
   puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
   order_number
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order1 = take_order(DRINKS)
+drinks = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
+foods = take_order(FOODS)
 
-total = DRINKS[order1][:price] + FOODS[order2][:price]
+total = DRINKS[drinks][:price] + FOODS[foods][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
-
